253a90a0486b43d7b811a0a963cd75afe92cf05c
AngelH2m <angelh2m@icloud.com>
2018-01-10 19:12:58 -0500

    V1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyBOZR97cQw4i3KPx2WcwHzP_tHHo9SMvHM",
+    authDomain: "angularshop-ab62e.firebaseapp.com",
+    databaseURL: "https://angularshop-ab62e.firebaseio.com",
+    projectId: "angularshop-ab62e",
+    storageBucket: "angularshop-ab62e.appspot.com",
+    messagingSenderId: "284244647505"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBOZR97cQw4i3KPx2WcwHzP_tHHo9SMvHM",
+    authDomain: "angularshop-ab62e.firebaseapp.com",
+    databaseURL: "https://angularshop-ab62e.firebaseio.com",
+    projectId: "angularshop-ab62e",
+    storageBucket: "angularshop-ab62e.appspot.com",
+    messagingSenderId: "284244647505"
+  }
 };
